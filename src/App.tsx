aed57ccfcf9f21aa3d114561d8659a2e6f0b1390
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-10-21 16:39:26 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Login } from './pages/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+      <div>
+        <Login/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+export const Login = () => {
+    return (
+        <div>        
+            <h1>Login</h1>
+
+            <input type="text" placeholder="login" />
+            <br />
+            <input type="password" placeholder="password" />
+            <br /><br />
+            <button>Login</button>
+        </div>
+
+    )
+}
