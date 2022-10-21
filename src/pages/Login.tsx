598579e4f2d9f628dd8053632a5ecfd5f82c581a
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-10-21 17:10:36 -0300

    Context and Navigate

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,30 @@
+import { useContext, useState } from "react"
+import {useNavigate} from "react-router-dom"
+
 export const Login = () => {
+const auth = useContext()
+    
+const [user,setUser] = useState('')
+const [password, setPassword] = useState('')
+
+const handleLogin = async () => {
+    if(user && password){
+        const isLogged = await auth.signin(user, password)
+        if(isLogged){
+            navigate('/')
+        }else {
+            alert("Não deu certo.")
+        }
+    }
+}  
+
     return (
         <div>        
             <h1>Login</h1>
 
-            <input type="text" placeholder="login" />
+            <input value={user} type="text" placeholder="login" />
             <br />
-            <input type="password" placeholder="password" />
+            <input value={password} type="password" placeholder="password" />
             <br /><br />
             <button>Login</button>
         </div>
